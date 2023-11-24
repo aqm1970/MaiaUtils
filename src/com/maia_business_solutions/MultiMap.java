ae85e6edf27555b0243945588042ba76e1650d92
Adrian Q Montgomery <aqm1970@yahoo.com>
2023-11-24 13:08:48 -0500

    Changes to support updated JDK

--- src/com/maia_business_solutions/MultiMap.java
+++ src/com/maia_business_solutions/MultiMap.java
@@ -1,5 +1,6 @@
 package com.maia_business_solutions;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -10,31 +11,32 @@ import java.util.Set;
 public class MultiMap<K, V> implements Map<K, Collection<V>>
 {
   private final Map<K, Collection<V>> internalMap;
-  
+
   private final Class<? extends Collection<V>> clazz;
-  
+
   public MultiMap()
   {
     this(new LinkedHashMap<>(), new ArrayList<>());
   }
-  
+
   public MultiMap(final Map<K, Collection<V>> map)
   {
     this(map, new ArrayList<>());
   }
-  
+
   public MultiMap(final Collection<V> collection)
   {
     this(new LinkedHashMap<>(), collection);
   }
-  
+
   @SuppressWarnings("unchecked")
-  public MultiMap(final Map<K, Collection<V>> map, final Collection<V> collection)
+  public MultiMap(final Map<K, Collection<V>> map,
+      final Collection<V> collection)
   {
     internalMap = map;
     clazz = (Class<? extends Collection<V>>) collection.getClass();
   }
-  
+
   @Override
   public int size()
   {
@@ -57,12 +59,12 @@ public class MultiMap<K, V> implements Map<K, Collection<V>>
   public boolean containsValue(Object value)
   {
     boolean retVal = false;
-    
+
     final Iterator<Collection<V>> iter = values().iterator();
-    
+
     while (retVal == false && iter.hasNext())
       retVal = iter.next().contains(value);
-    
+
     return retVal;
   }
 
@@ -71,21 +73,24 @@ public class MultiMap<K, V> implements Map<K, Collection<V>>
   {
     return internalMap.get(key);
   }
-  
+
   public boolean put(K key, V value)
   {
     Collection<V> values = get(key);
-    
+
     if (values == null) {
       try {
-        values = clazz.newInstance();
+        values = clazz.getDeclaredConstructor().newInstance();
         put(key, values);
       }
-      catch (InstantiationException | IllegalAccessException e) {
+      catch (InstantiationException | IllegalAccessException
+          | IllegalArgumentException | InvocationTargetException
+          | NoSuchMethodException | SecurityException e)
+      {
         e.printStackTrace();
       }
     }
-    
+
     return values.add(value);
   }
 
